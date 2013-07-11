ec7368e539ee46e8c728117b5d3256ad32030f4d
Dave Preece <davep@iweb.co.uk>
2013-07-11 23:15:22 +0100

    Core files

---
+++ davepreece-core.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Dave Preece Core Classes - Forms
+Plugin URI: http://carbolowdrates.com
+Description: Core classes and dependencies
+Version: 0.1
+Author: Dave Preece
+Author URI: http://www.scumonline.co.uk
+License: GPL
+
+Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : dangerous@scumonline.co.uk)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+spl_autoload_register(function($class){
+	if(substr($class, 0, 3) == "DP_"){
+		if(file_exists(dirname(__FILE__) . '/DP/' . $class . ".php")){
+			include('DP/' . $class . ".php");
+		}
+	}
+});
